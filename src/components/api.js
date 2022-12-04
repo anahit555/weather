ddb16e90df4306968225e64b5531b02cd386fa2b
anahit555 <anahit.asatryan.95@mail.ru>
2022-12-04 12:12:37 +0400

    1 commit

---
+++ src/components/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ac70251caamsh48f747abe490fa1p1aaa9cjsn6defe8a732cc',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
